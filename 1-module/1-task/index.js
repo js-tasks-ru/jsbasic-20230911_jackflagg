db973c04feb48eab4ca53ac7e4db04c24521ce79
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-09-17 19:29:18 +0500

    решил одну из пяти задач - факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,22 @@
 function factorial(n) {
-  // ваш код...
+  let i = 0 //переменная счетчика
+  let result = 1 // переменная для хранения полученного рез-та
+  let message = '' //переменная вывода для некорректных значений
+  if (n>0) {
+    while (i < n){
+      result *= 1*(n-i)
+      i++
+    };
+    return result
+  } else if (n === 0) {
+    return 1
+  } else if (n < 0){
+    message = 'Введите значение строго больше нуля!'
+    return message
+  } else if (typeof n != 'number'){
+    message = 'Вы ввели нечисловое значение! Попробуйте еще раз'
+    return message
+  }
 }
+
+console.log(factorial(8))
