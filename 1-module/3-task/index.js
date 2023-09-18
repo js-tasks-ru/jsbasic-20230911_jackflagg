58804e449420c0be1b03bb342cd977896274a091
Rasul Khamzin <rasul.khamzinnn@gmail.com>
2023-09-18 13:56:07 +0500

    решил 3 задачу второго занятия

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,12 @@
-function ucFirst(str) {
-  // ваш код...
+function ucFirst(str){
+  if (str === ''){
+    return ''
+  } else {
+  const peremTwo = str[0].toUpperCase();
+  const stringNew = str.replace(str[0], peremTwo)
+  return stringNew;
+    }
 }
+
+let peremOne = ucFirst('')
+console.log(peremOne)
